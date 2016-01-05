4bf0155e4ebd41fa986836c44ce0d9505d6a327b
Beat Christen <beat.christen@systarch.com>
2016-01-05 09:44:40 +0100

    install modules/simp_le only if missing

--- manifests/install.pp
+++ manifests/install.pp
@@ -10,22 +10,38 @@ class letsencrypt::install {
     path    => ["/usr/bin"]
   }
 
+  exec { 'funcsigs-for-letsencrypt':
+    command => 'pip install -U funcsigs',
+    cwd     => $::letsencrypt::install_dir,
+    path    => [ "${::letsencrypt::install_dir}/bin", '/usr/bin'],
+    unless  => 'python -c "import funcsigs"',
+    require => Exec['virtualenv-for-letsencrypt'],
+    before  => Exec['simp_le'],
+  }
+
   exec { 'setuptools-for-letsencrypt':
     command => 'pip install -U setuptools',
     cwd     => $::letsencrypt::install_dir,
     path    => [ "${::letsencrypt::install_dir}/bin", '/usr/bin'],
+    unless  => 'python -c "import setuptools"',
+    require => Exec['virtualenv-for-letsencrypt'],
+    before  => Exec['simp_le'],
   }
 
   exec { 'wheel-for-letsencrypt':
     command => 'pip install -U wheel',
     cwd     => $::letsencrypt::install_dir,
     path    => [ "${::letsencrypt::install_dir}/bin", '/usr/bin'],
+    unless  => 'python -c "import wheel"',
+    require => Exec['virtualenv-for-letsencrypt'],
+    before  => Exec['simp_le'],
   }
 
   exec { 'simp_le':
     command => 'pip install -U git+https://github.com/kuba/simp_le.git@master',
     cwd     => $::letsencrypt::install_dir,
     path    => [ "${::letsencrypt::install_dir}/bin", '/usr/bin'],
+    unless  => 'python -c "import simp_le"',
   }
 
 }
