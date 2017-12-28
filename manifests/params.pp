a7b4c9fecb824fc24166e4cd68be27250aff1c96
krebsst <stefan.krebs@srf.ch>
2017-12-28 10:15:54 +0100

    acceppt new ToS v 1.2i - https://letsencrypt.org/repository/
    NOREF

--- manifests/params.pp
+++ manifests/params.pp
@@ -38,5 +38,6 @@ class letsencrypt::params {
   $valid_days = 40
 
   # see https://github.com/kuba/simp_le/issues/114#issuecomment-236744611
-  $tos_sha256 = '6373439b9f29d67a5cd4d18cbc7f264809342dbf21cb2ba2fc7588df987a6221'
+  # and https://github.com/JrCs/docker-letsencrypt-nginx-proxy-companion/issues/277
+  $tos_sha256 = 'cc88d8d9517f490191401e7b54e9ffd12a2b9082ec7a1d4cec6101f9f1647e7b'
 }
